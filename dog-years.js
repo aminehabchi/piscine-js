52574b9395b4909b8b96471e89fa3cd6f9933744
ahabchi <amine.habchi.01@gmail.com>
2024-09-30 14:24:34 +0100

    js

--- dog-years.js
+++ dog-years.js
@@ -1,27 +1,22 @@
-function dogYears(name,age){
-    let a=age*7/31557600 
-    if (name=="earth"){
-        return a.toFixed(2)
-    }else if (name=="mercury"){
-        a*=0.2408467
-        return a.toFixed(2)
-    }else if (name=="venus"){
-        a*=0.61519726
-        return a.toFixed(2)
-    }else if (name=="mars"){
-        a*=1.8808158
-        return a.toFixed(2)
-    }else if (name=="saturn"){
-        a*=29.447498
-        return age.toFixed(2)
-    }else if (name=="uranus"){
-        a*=84.016846
-        return a.toFixed(2)
-    }else if (name=="neptune"){
-        a*=164.79132
-        return a.toFixed(2)
-    }else if (name=="jupiter"){
-        a*=11.862615 
-        return a.toFixed(2)
-    }
+function dogYears(planet, age) {
+
+    const planetName = {
+        earth: 1.0,
+        mercury: 0.2408467,
+        venus: 0.61519726,
+        mars: 1.8808158,
+        jupiter: 11.862615,
+        saturn: 29.447498,
+        uranus: 84.016846,
+        neptune: 164.79132
+    };
+
+
+    let humanYears = (age / (31557600 * planetName[planet]));
+
+   
+    let dogYears = humanYears * 7;
+
+ 
+    return dogYears.toFixed(2);
 }
