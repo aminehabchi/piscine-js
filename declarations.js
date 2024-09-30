77ce280c3534731cf8d9b07bef1a5d2e202fbae6
ahabchi <amine.habchi.01@gmail.com>
2024-09-30 12:07:31 +0100

    done

--- declarations.js
+++ declarations.js
@@ -9,20 +9,24 @@ const obj={
      bool:false,
      undef:undefined,
 };
-Object.freeze(obj)
+Object.freeze(obj);
 const obj1={
     str:'',
     num:0,
     bool:false,
 };
+
 Object.freeze(obj1)
+
 const nested={
     arr:[4,undefined,'2'],
      obj:obj1
 };
 
+
+
 Object.freeze(nested.arr);
 
-Object.freeze(nested);
-
 Object.freeze(nested.obj);
+
+Object.freeze(nested);
