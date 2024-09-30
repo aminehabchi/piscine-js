327c62e1c0193cd17a9e68eb040fa87f5b8c3e59
ahabchi <amine.habchi.01@gmail.com>
2024-09-30 13:57:41 +0100

    js

---
+++ dog-years.js
@@ -0,0 +1,14 @@
+function dogYears(name,age){
+    var y =age*7/31557600 
+    planet={
+        earth:y,
+        mercury:y*0.2408467,
+        venus:y*0.61519726,
+        mars:y*1.8808158,
+        jupiter:y*11.862615,
+        saturn:y*29.447498,
+        uranus:y*84.016846,
+        neptune:y*164.79132,
+    }
+    return planet[name];
+}
