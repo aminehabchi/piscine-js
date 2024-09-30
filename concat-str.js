d61a13b9f7b3ab5422c6ffad7e091995656de634
ahabchi <amine.habchi.01@gmail.com>
2024-09-30 13:35:23 +0100

    js

--- concat-str.js
+++ concat-str.js
@@ -2,6 +2,6 @@ function concatStr(a,b){
    /* if (typeof a==Number){
         return Number(a)+Number(b)
     }*/
-    return String(a+b);
+    return String(a)+String(b);
 }
 //console.log(concatStr("1","2"))
