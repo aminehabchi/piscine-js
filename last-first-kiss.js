fe38b9698b861e48a892a2817c3ddb3ff0704831
ahabchi <amine.habchi.01@gmail.com>
2024-09-30 13:24:20 +0100

    js

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,22 +1,10 @@
 function first(s){
-    if (s.length==0){
-        return undefined;
-    }
     return s[0]
 }
 function last(s){
-    if (s.length==0){
-        return undefined;
-    }
     return s[s.length-1];
 }
 function kiss(s){
-    if (s.length==0){
-        return undefined;
-    }
-    if (s.length==1){
-        return [s[0]];
-    }
     var arr=[s[s.length-1],s[0]]
     return arr;
 }
