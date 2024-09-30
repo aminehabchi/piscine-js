110562c8067aa05dc2208a44bce44b65befebee3
ahabchi <amine.habchi.01@gmail.com>
2024-09-30 13:08:57 +0100

    f

--- mutability.js
+++ mutability.js
@@ -1,8 +1,4 @@
-// const person = {
-//     name: 'Rick',
-//     age: 77,
-//     country: 'US',
-//   }
+
 const clone1 = Object.assign({}, person);
 const clone2 = Object.assign({}, person);
 
@@ -10,8 +6,3 @@ const samePerson = person;
 
 person.country='FR'
 person.age++
-
-// console.log(person)
-// console.log(clone1)
-// console.log(clone2)
-// console.log(samePerson)
