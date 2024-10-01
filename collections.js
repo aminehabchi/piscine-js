0022a0765283d020813be96fddb8b89614119a8a
ahabchi <amine.habchi.01@gmail.com>
2024-10-01 11:48:09 +0100

    done

--- collections.js
+++ collections.js
@@ -1,15 +1,10 @@
+// Your function definitions
 const arrToSet = (arr) => new Set(arr);
-
 const arrToStr = (arr) => arr.join('');
-
 const setToArr = (set) => Array.from(set);
-
 const setToStr = (set) => Array.from(set).join('');
-
 const strToArr = (str) => Array.from(str);
-
 const strToSet = (str) => new Set(Array.from(str));
-
 const mapToObj = (map) => {
     const obj = {};
     for (let [key, value] of map) {
@@ -17,11 +12,8 @@ const mapToObj = (map) => {
     }
     return obj;
 };
-
 const objToArr = (obj) => Object.values(obj);
-
 const objToMap = (obj) => new Map(Object.entries(obj));
-
 const arrToObj = (arr) => {
     const obj = {};
     arr.forEach((value, index) => {
@@ -29,7 +21,6 @@ const arrToObj = (arr) => {
     });
     return obj;
 };
-
 const strToObj = (str) => {
     const obj = {};
     Array.from(str).forEach((char, index) => {
@@ -37,3 +28,20 @@ const strToObj = (str) => {
     });
     return obj;
 };
+
+const superTypeOf = (value) => {
+    if (value === null) return 'null';
+    if (Array.isArray(value)) return 'Array';
+    return Object.prototype.toString.call(value).slice(8, -1);
+};
+
+// Example Test Function
+const testSuperTypeOf = ({ eq }) => {
+    eq(superTypeOf(new Map()), 'Map');
+};
+
+// Example of eq function for testing
+const eq = (a, b) => a === b;
+
+// Run the test
+testSuperTypeOf({ eq });
