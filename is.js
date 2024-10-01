42227cdd27a0009c851d65cbc3de0513e527a2b0
ahabchi <amine.habchi.01@gmail.com>
2024-10-01 10:49:23 +0100

    done

--- is.js
+++ is.js
@@ -1,77 +1,11 @@
-is.num=isInteger
-is.nan=isNaN
-is.str=isString
-is.bool=isBool
-is.undef=isUndef
-is.def=isdef
-is.arr=isarr
-is.obj=isobj
-is.fun=isfunc
-is.truthy=istruthy
-is.falsy=isfalsy
-
-function isInteger(s){
-   
-    return Number.isInteger
-}
-
-function isNaN(s){
-    if (typeof(s)==NaN){
-        return true
-    }
-    return false
-}
-function isfalsy(s){
-    if (typeof(s)=='falsy'){
-        return true
-    }
-    return false
-}
-function istruthy(s){
-    if (typeof(s)=='truthy'){
-        return true
-    }
-    return false
-}
-function isfunc(s){
-    if (typeof(s)=='function'){
-        return true
-    }
-    return false
-}
-function isobj(s){
-    if (typeof(s)==Object){
-        return true
-    }
-    return false
-}
-function isarr(s){
-    if (typeof(s)==Array){
-        return true
-    }
-    return false
-}
-function isdef(s){
-    if (typeof(s)=='defined'){
-        return true
-    }
-    return false
-}
-function isUndef(s){
-    if (typeof(s)==undefined){
-        return true
-    }
-    return false
-}
-function isString(s){
-    if (typeof(s)==String){
-        return true
-    }
-    return false
-}
-function isBool(s){
-    if (typeof(s)==Boolean){
-        return true
-    }
-    return false
-}
+is.num = (a) => typeof a === 'number'
+is.nan = (a) => Number.isNaN(a)
+is.str = (a) => typeof a === "string"
+is.bool = (a) => typeof a === 'boolean'
+is.undef = (a) => typeof a === "undefined"
+is.def = (a) => typeof a !== "undefined"
+is.arr = (a) => Array.isArray(a)
+is.obj = (a) => typeof a === "object" && a != null && !Array.isArray(a)
+is.fun = (a) => typeof a === 'function'
+is.truthy = (a) => Boolean(a) === true
+is.falsy = (a) => Boolean(a) === false
