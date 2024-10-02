03ba0405d9b4f047641f2f15049e2e1af4df6a38
ahabchi <amine.habchi.01@gmail.com>
2024-10-02 17:10:14 +0100

    js

--- unbreakable.js
+++ unbreakable.js
@@ -1,30 +1,45 @@
-function split(s, sub) {
-    if (s === ''){
-        return [''];
-    } 
-    if (sub === ''){
-       return [s];  
-    } 
-    
-    const arr = [];
-    let currentSegment = '';
-    let index = 0;
-
-    while (index < s.length) {
-        if (s.slice(index, index + sub.length) === sub) {
-            arr.push(currentSegment);
-            currentSegment = '';
-            index += sub.length; 
-        } else {
-            currentSegment += s[index]; 
-            index++;
+function split(s,sub){
+    if (s==sub){
+        return ['']
+    }
+    const arr=[]
+    let a=""
+    while (s.length!==0){
+        if (s.length>=sub.length && keepFirst(s,sub.length)==sub){
+            arr.push(a)
+            s=remove(s,sub.length)
+            a=""
+            if (s===''){
+                arr.push(a)
+            }
+            
+        }else{
+            a=a+s[0]
+            s=remove(s,1)
         }
     }
-    
-    if (currentSegment) {
-        arr.push(currentSegment);
+    if (a!==""){
+        arr.push(a)
     }
-    return arr;
+    return arr
+}
+//console.log(split('ee,ff,g,', ','))
+function remove(s,k){
+    let ss=""
+    for (let i=k;i<s.length;i++){
+        ss=ss+s[i]
+    }
+    return ss
+}
+function keepFirst(s,j){
+    if (j>s.length){
+        return ''
+    }
+    let ss=""
+    for (let i=0;i<j;i++){
+        ss=ss+s[i]
+    }
+    return ss
 }
 function join(s,sub){
     let ss="";
