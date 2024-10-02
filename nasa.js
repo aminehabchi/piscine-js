99ae7bffb2d143eb19799d78a6cd4ac899010eac
ahabchi <amine.habchi.01@gmail.com>
2024-10-02 17:29:01 +0100

    js

--- nasa.js
+++ nasa.js
@@ -1,11 +1,11 @@
 function nasa(N){
     let o=""
-    for (let i=0;i<=N;i++){
-        if (i!=0 && i%3==0 && i%5==0){
+    for (let i=1;i<=N;i++){
+        if (i%3==0 && i%5==0){
             o=o+"NASA"
-        }else if (i!=0 && i%3==0){
+        }else if (i%3==0){
             o=o+"NA"
-        }else if (i!=0 && i%5==0){
+        }else if (i%5==0){
             o=o+"SA"
         }else{
             o=o+i
