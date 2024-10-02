5c0155dc88394332f06696b0c50c58aafc762fbb
ahabchi <amine.habchi.01@gmail.com>
2024-10-02 14:57:48 +0100

    js

--- cut-corners.js
+++ cut-corners.js
@@ -1,91 +1,67 @@
-function findNearest(x) {
-    let num = 0;
-    if (x < 1 && x >= 0) return 0
-    if (x < 0 && x >= -1) return 0
-    for (let i = x; i > 1; i = i / 2) {
-        num += 1;
+
+// const nums = [3.7, -3.7, 3.1, -3.1]
+// console.log(nums.map(round))
+// console.log(nums.map(floor))
+// console.log(nums.map(trunc))
+// console.log(nums.map(ceil))
+
+function ceil(n){
+    if (get(n)==0){
+        return n
     }
-    return num - 1
+    if (n<0){
+        return n+get(n)
+    }
+    return n-get(n)+1
+}
+function floor(n){
+    if (get(n)==0){
+        return n
+    }
+    if (n<0){
+        return n+get(n)-1
+    }
+    return n-get(n)
 }
 
-function trunc(x) {
-    let i = 2 ** findNearest(x);
-    if (x >= 0) {
-        for (; i < x;) {
-            i++
+function round(n){
+    if (n<0){
+        let a=get(n)
+        if (a*-1>=-0.5){
+            return trunc(n)
         }
-        if (i === x) return i
-        return i - 1
+        return trunc(n)-1
     }
-    if (x < 0) {
-        i = 0
-        for (; i > x;) {
-            i--
-        }
-        if (x > -1) return -0
-        if (i === x) return i
-        return i + 1
+    if (get(n)>=0.5){
+        return trunc(n)+1
     }
+    return trunc(n)
 }
-
-function floor(x) {
-    let i = 0;
-    if (x >= 0) {
-        for (; i < x;) {
-            i++
-        }
-        if (i === x) return i
-        return i - 1
+//console.log(get(-3.7))
+function trunc(a) {
+    if (a>-1 && a<1){
+        return 0
     }
-    if (x < 0) {
-        i = 0
-        for (; i > x;) {
-            i--
-        }
-        if (x === -0) return -0
-        if (i === x) return i
-        return i
+    if (a<0){
+        return a+get(a)
     }
+    return a-get(a)
 }
-
-function ceil(x) {
-    let i = 0;
-    if (x >= 0) {
-        for (; i < x;) {
-            i++
-        }
-        if (i === x) return i--
-        return i
+function get(a){
+    if (a>-1 && a<1){
+        return a
     }
-    if (x < 0) {
-        i = 0
-        for (; i > x;) {
-            i--
-        }
-        if (i === x) return i
-        if (i + 1 === 0) return -0
-        return i + 1
+    if (a<0){
+        a=a*-1
     }
-}
-
-function round(x) {
-    let i = 0;
-    if (x >= 0) {
-        for (; i < x;) {
-            i++
+    while (a/10<0){
+        a/=10
+    } 
+    while (a>0){
+        if (a-1<0){
+            break
         }
-        if (i-x > 0.5) return i - 1
-        if (i-x <= 0.5) return i
-    }
-    if (x < 0) {
-        i = 0
-        for (; i > x;) {
-            i--
-        }
-        if (i-x > -0.5) return i
-        if (i-x <= 0.5) {
-            if (i + 1 === 0) return -0
-            return i + 1
-        }
-    }
+        a-=1
+    } 
+    return a
 }
