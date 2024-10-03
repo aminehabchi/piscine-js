2b4b85611fd9cf9b35876c6039e877893d2c7e6f
ahabchi <amine.habchi.01@gmail.com>
2024-10-03 11:29:40 +0100

    js

--- pyramid.js
+++ pyramid.js
@@ -1,16 +1,21 @@
 function pyramid(s,n){
     let o=""
+    let k=1
     for (let i=0;i<n;i++){
         for (let j=0;j<n-i-1;j++){
-            o=o+" "
+            for (let k=0;k<s.length;k++){
+                 o=o+" "
+            }
+
         }
-        for (let j=0;j<=i*2;j++){
+        for (let j=0;j<k;j++){
             o=o+s
         }
-        if (i!=n-1){
+       if (i!=n-1){
             o=o+"\n"
-        }
-        
+       }
+       k+=2
     }
+    
     return o
 }
