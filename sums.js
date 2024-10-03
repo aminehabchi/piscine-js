5ddd146091ec529b4ed736539f2cad266dce4bd2
ahabchi <amine.habchi.01@gmail.com>
2024-10-03 14:51:45 +0100

    js

--- sums.js
+++ sums.js
@@ -1,41 +1,45 @@
+const result = [];
 
-
-const result = []
-function sums(n,arr=[],k=1) {
-    let l = arr.length
-    let a=[]
+function sums(n, arr = [], k = 1) {
+    let a = [];
+    
     for (let i = 1; i < n; i++) {
-        for (let j = 0; j < l; j++) {
+        for (let j = 0; j < arr.length; j++) {
             if (calcule(arr[j]) <= n) {
-                let copiedArray = [...arr[j]]
-                copiedArray.push(i)
-                if (calcule(arr[j]) == n){
-                    arr[j].sort()
-                    result.push(arr[j])
-                }else{
-                    a.push(copiedArray)
+                let copiedArray = [...arr[j]];
+                copiedArray.push(i);
+                
+                copiedArray.sort((a, b) => a - b)
+                
+                if (calcule(copiedArray) === n) {
+                    result.push(copiedArray);
+                } else if (calcule(copiedArray) < n) {
+                    a.push(copiedArray);
                 }
             }
         }
-        a.push([i])
+        a.push([i]);
     }
-    arr=a
-    //console.log(a)
-    if (k<n*n) {
-        sums(n,arr,k+1)
+    
+    arr = a;
+    
+    if (k < n) {
+        sums(n, arr, k + 1);
     }
-    return removeDuplicateSubarrays(result)
+
+    return removeDuplicateSubarrays(result);
 }
+
+
 function calcule(arr) {
-    let sum = 0
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i]
-    }
-    return sum
+    return arr.reduce((sum, value) => sum + value, 0);
 }
-// console.log(sums(5))
+
+
 function removeDuplicateSubarrays(arr) {
     const uniqueSubarrays = new Set(arr.map(subArray => JSON.stringify(subArray)));
     return Array.from(uniqueSubarrays).map(subArray => JSON.parse(subArray));
 }
 
+
+//console.log(sums(6));  
