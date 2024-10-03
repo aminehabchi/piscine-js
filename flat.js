ce5509bb4322a73d183d0576b6b5fad43e889031
ahabchi <amine.habchi.01@gmail.com>
2024-10-03 11:46:02 +0100

    js

--- flat.js
+++ flat.js
@@ -1,12 +1,16 @@
 
 var myarr=[]
-function flat(arr){
+function flat(arr,j){
+    if (j===undefined){
+        j=Infinity
+    }
     for (let i=0;i<arr.length;i++){
-        if (Array.isArray(arr[i])){
-            myarr=flat(arr[i])
+        if (Array.isArray(arr[i]) && j!=0){
+            myarr=flat(arr[i],j-1)
         }else{
             myarr.push(arr[i])
         }
     }
     return myarr
 }
+//console.log(flat([1, [2]]))
