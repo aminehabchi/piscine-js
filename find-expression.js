c4d8d511660b5038e5c78903090ff1130a252ff7
ahabchi <amine.habchi.01@gmail.com>
2024-10-03 12:44:56 +0100

    js

--- find-expression.js
+++ find-expression.js
@@ -1,17 +1,17 @@
 
 function findExpression(num,n=1,s="1"){
     if (n>num){
-        return 'undefined'
+        return undefined
     }
     if (n==num){
         return s
     }
     let a=findExpression(num,n+4,s+" "+add4)
-    if (a!='undefined'){
+    if (a!=undefined){
         return a
     }
     a=findExpression(num,n*2,s+" "+mul2)
-    if (a!='undefined'){
+    if (a!=undefined){
         return a
     }
     return undefined;
