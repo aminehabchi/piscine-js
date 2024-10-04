0c9c85229d1caed1e86c407793208244f51108b1
ahabchi <amine.habchi.01@gmail.com>
2024-10-04 20:55:47 +0100

    js

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,9 +1,8 @@
 function letterSpaceNumber(s) {
-    const myregex = /[a-zA-Z]\s\d(?=\W)/g
+    const myregex = /[a-zA-Z]\s\d(?!\w)/g
    const m=s.match(myregex)
    if (m==null){
     return []
    }
     return  m
 }
-// console.log(letterSpaceNumber("I like 7up."))
