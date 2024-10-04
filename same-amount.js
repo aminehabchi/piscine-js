9439f45e336fbd551c94a0227917e0f596f7a1f9
ahabchi <amine.habchi.01@gmail.com>
2024-10-04 21:31:34 +0100

    js

--- same-amount.js
+++ same-amount.js
@@ -2,17 +2,11 @@ function sameAmount(s,r1,r2){
     if (typeof(s)!='string'){
         return false
     }
-    if (letterSpaceNumber(s,r1).length==letterSpaceNumber(s,r2).length){
+    const m=s.match(r1)
+    const m1=s.match(r2)
+    if (m.length==m.length){
         return true
     }
     return false
 }
-function letterSpaceNumber(s,myregex) {
-    const m=s.match(myregex)
-    if (m==null){
-     return []
-    }
-     return  m
- }
-//  const data=[]
-//  console.log(sameAmount(data, /q /, /qqqqqqq/))
+
