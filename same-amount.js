714c9266acce49274bd345ee009dac4d303f0d9e
ahabchi <amine.habchi.01@gmail.com>
2024-10-04 21:20:23 +0100

    js

---
+++ same-amount.js
@@ -0,0 +1,13 @@
+function sameAmount(s,r1,r2){
+    if (letterSpaceNumber(s,r1).length==letterSpaceNumber(s,r2).length){
+        return true
+    }
+    return false
+}
+function letterSpaceNumber(s,myregex) {
+    const m=s.match(myregex)
+    if (m==null){
+     return []
+    }
+     return  m
+ }
--- vowel-dots.js
+++ vowel-dots.js
@@ -2,4 +2,3 @@ var vowels=/[a|e|i|o|u|A|E|I|O|U]/g
 function vowelDots(s){
    return s.replace(vowels,"$&.")
 }
-
