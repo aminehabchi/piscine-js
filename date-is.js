b1e0773b273d03af40f0dbf0b15a5b9f97e8ef48
ahabchi <amine.habchi.01@gmail.com>
2024-10-08 11:19:29 +0100

    done

--- date-is.js
+++ date-is.js
@@ -1,10 +1,10 @@
 function isValid(date){
-    if (date-10===NaN){
+    if (isNaN(date)){
         return false
     }
     return true
 }
-console.log((new Date('')-10))
+console.log(isValid(new Date('')))
 
 function isAfter(d1,d2){
     if (d1-1<d2-1){
