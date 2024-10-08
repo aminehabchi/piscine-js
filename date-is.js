05883767d45d84937f62b4de697cadedb5d5260c
ahabchi <amine.habchi.01@gmail.com>
2024-10-08 11:21:53 +0100

    done

--- date-is.js
+++ date-is.js
@@ -1,12 +1,15 @@
 function isValid(date){
-    if (isNaN(date)){
+    if (isNaN(date) || typeof(date)=='string'){
         return false
     }
     return true
 }
-console.log(isValid(new Date('')))
+console.log(isValid(''))
 
 function isAfter(d1,d2){
+    if (!isValid(d1) || !isValid(d2)){
+        return false
+    }
     if (d1-1<d2-1){
         return true
     }
@@ -14,12 +17,18 @@ function isAfter(d1,d2){
 }
 
 function isBefore(d1,d2){
+    if (!isValid(d1) || !isValid(d2)){
+        return false
+    }
     if (d1-1>d2-1){
         return true
     }
     return false
 }
 function isPast(date){
+    if (!isValid(date)){
+        return false
+    }
     const n = Date.now()
     if (n-date>0){
         return true
