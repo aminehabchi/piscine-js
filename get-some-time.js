014cc52f3d23a02e0ba381e7d4ed7cb78e029abf
ahabchi <amine.habchi.01@gmail.com>
2024-10-08 13:17:16 +0100

    doen

--- get-some-time.js
+++ get-some-time.js
@@ -1,14 +1,19 @@
 function firstDayWeek(weekNum, yearNum) {
 
-    let year = new Date(yearNum, 0, 1);
-    let adj = (year.getDay() === 0 ? 6 : year.getDay() - 1); 
-    year.setDate(year.getDate() - adj);
-    let firsOfWeek = new Date(year);
-    firsOfWeek.setDate(firsOfWeek.getDate() + 7 * (weekNum - 1));
-    if (firsOfWeek.getFullYear() < yearNum) {
+    let yearStart = new Date(yearNum, 0, 1);
+
+    let dayOfWeek = (yearStart.getDay() === 0 ? 6 : yearStart.getDay() - 1); 
+
+    yearStart.setDate(yearStart.getDate() - dayOfWeek);
+
+    let firstOfWeek = new Date(yearStart);
+    firstOfWeek.setDate(firstOfWeek.getDate() + 7 * (weekNum - 1));
+
+    if (firstOfWeek.getFullYear() < yearNum) {
         return '01-01-' + yearNum;
     }
-    return firsOfWeek.getDate().toString().padStart(2, '0') + '-' +
-        (firsOfWeek.getMonth() + 1).toString().padStart(2, '0') + '-' +
-        firsOfWeek.getFullYear().toString().padStart(4, '0');
+
+    return firstOfWeek.getDate().toString().padStart(2, '0') + '-' +
+           (firstOfWeek.getMonth() + 1).toString().padStart(2, '0') + '-' +
+           firstOfWeek.getFullYear();
 }
