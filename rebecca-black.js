490c6c24670e72dc592ec7924bb8dd2ac80a2521
ahabchi <amine.habchi.01@gmail.com>
2024-10-08 13:46:00 +0100

    js

--- rebecca-black.js
+++ rebecca-black.js
@@ -1,32 +1,34 @@
 function isFriday(date){
-    if (date.day==4){
+    if (date.getDay()==4){
         return true
     }
     return false
 }
 function isWeekend(date){
-    if (date.day==5 || date.day==6){
+    if (date.getDay()==5 || date.getDay()==6){
         return true
     }
     return false
 }
 function isLeapYear(date){
-    let year=date.getYear()
+    let year=date.getYear()+1900
     if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
         return true
     }
     return false
 }
-function isLastDayOfMonth(date){
-    let d=new Date(date.getYear(),date.getMonth(),1)
-    let def=d-date
-    def=def/(1000*60*60*24)
-    const m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    if (isLeapYear(year)) {
-        m[1] = 29;
-    }
-    if (m[date.getMonth()]==def){
-        return true
-    }
-    return false
-}
+// function isLastDayOfMonth(date){
+//     let d=new Date(date.getYear(),date.getMonth(),1)
+//     let def=d
+//     def=def/(1000*60*60*24)
+//     console.log(def)
+//     const m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+//     if (isLeapYear(date)) {
+//         m[1] = 29;
+//     }
+//     if (m[date.getMonth()]==def){
+//         return true
+//     }
+//     return false
+// }
+// console.log(isLastDayOfMonth(new Date('2016-01-1')))
