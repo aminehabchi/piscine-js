18d370c9e3d867b9335e90ac33fb5e9ea824d369
ahabchi <amine.habchi.01@gmail.com>
2024-10-08 15:15:45 +0100

    js

--- match-cron.js
+++ match-cron.js
@@ -3,16 +3,16 @@ function matchCron(s,date){
    if (s[0]!="*" && Number(s[0])!=date.getMinutes()){
     return false
    }
-   if (s[1]!="*" && Number(s[0])!=date.getHours()){
+   if (s[1]!="*" && Number(s[1])!=date.getHours()){
     return false
    }
-   if (s[2]!="*" && Number(s[0])!=date.getDate()){
+   if (s[2]!="*" && Number(s[2])!=date.getDate()){
     return false
    }
-   if (s[3]!="*" && Number(s[0])!=date.getMonth()){
+   if (s[3]!="*" && Number(s[3])!=date.getMonth()){
     return false
    }
-   if (s[4]!="*" && Number(s[0])!=date.getDay()){
+   if (s[4]!="*" && Number(s[4])!=date.getDay()){
     return false
    }
    return true
@@ -20,3 +20,4 @@ function matchCron(s,date){
 // console.log(matchCron('9 * * * *', new Date('2020-05-30 19:09:00')))
 // console.log(matchCron('9 * * * *', new Date('2020-05-30 19:09:00')))
 // console.log(matchCron('9 * * * *', new Date('2020-05-30 19:21:00')))
+// console.log(matchCron('* * * * 1', new Date('2020-06-01 00:00:00')))
