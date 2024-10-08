5c0f4972b90d1da02f4a25905209539396e07cc4
ahabchi <amine.habchi.01@gmail.com>
2024-10-08 15:47:05 +0100

    js

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -3,7 +3,11 @@ function dayOfTheYear(date){
     if (y==0){
         y++
     }
-    let d=new Date(y,0,1)
+    let d=new Date()
+    d.setFullYear(y)
+    d.setMonth(0)
+    d.setDate(0)
+    console.log(d)
     let def=date-d
     return Math.abs(Math.floor(def/(1000*60*60*24)+1))
 }
