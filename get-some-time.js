25b3998e5973ba57f56f80ad9025990e02dc7fd7
ahabchi <amine.habchi.01@gmail.com>
2024-10-08 12:27:59 +0100

    done

--- get-some-time.js
+++ get-some-time.js
@@ -1,5 +1,5 @@
 function firstDayWeek(n,s){
-    n=n*7 -6
+    n=n*7 
     const m=[31,28,31,30,31,30,31,31,30,31,30,31]
     if (isLeapYear(s)){
         m[1]=29
@@ -13,15 +13,44 @@ function firstDayWeek(n,s){
         n*=-1
     }
     j+=1
-    if (j<10){
-        j="0"+j
+    const date=new Date()
+    date.setDate(n)
+    date.setMonth(j)
+    date.setFullYear(s)
+   let day =n-date.getDay()-3
+    if (day<0){
+        if (j==1){
+            n=1
+        }else{
+            j--
+            n=m[j]+n
+        }
+    }else{
+        n=day
     }
     if (n<10){
         n="0"+n
     }
-    //console.log(n,j)
+    if (j<10){
+        j="0"+j
+    }
     return n+"-"+j+"-"+s
 }
+//  console.log(firstDayWeek(2, '0001')) //8
+// console.log(firstDayWeek(52, '1000')) //'22-12-1000'
+
+
+
+
+
+
+
+
+
+
+
+
+
 function isLeapYear(year) {
     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
       return true;
@@ -29,4 +58,3 @@ function isLeapYear(year) {
       return false;
     }
   }
-// console.log(firstDayWeek(2, '0001'))
