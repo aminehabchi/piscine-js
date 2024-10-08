53aace4364a04aa0a2033f1df39bd6f7bc426981
ahabchi <amine.habchi.01@gmail.com>
2024-10-08 13:13:50 +0100

    doen

--- get-some-time.js
+++ get-some-time.js
@@ -1,44 +1,14 @@
-function isLeapYear(year) {
-    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+function firstDayWeek(weekNum, yearNum) {
+
+    let year = new Date(yearNum, 0, 1);
+    let adj = (year.getDay() === 0 ? 6 : year.getDay() - 1); 
+    year.setDate(year.getDate() - adj);
+    let firsOfWeek = new Date(year);
+    firsOfWeek.setDate(firsOfWeek.getDate() + 7 * (weekNum - 1));
+    if (firsOfWeek.getFullYear() < yearNum) {
+        return '01-01-' + yearNum;
+    }
+    return firsOfWeek.getDate().toString().padStart(2, '0') + '-' +
+        (firsOfWeek.getMonth() + 1).toString().padStart(2, '0') + '-' +
+        firsOfWeek.getFullYear().toString().padStart(4, '0');
 }
-
-function firstDayWeek(n, s) {
-    if (n==1){
-        return '01-01-'+s
-    }
-    const year = parseInt(s);
-    const m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    if (isLeapYear(year)) {
-        m[1] = 29;
-    }
-
-    const d = new Date(year, 0, 1); 
-    let firstWeekDay = d.getDay(); 
-
-    
-    if (firstWeekDay !== 0) {
-        n -= 1;
-        m[0] -= firstWeekDay; 
-    }
-
-    n *= 7; 
-
-    let month = 0;
-    while (n > m[month]) {
-        n -= m[month]; 
-        month++;
-    }
-
-    d.setDate(n);
-    d.setMonth(month); 
-    d.setFullYear(year);
-
-    const day = d.getDate().toString().padStart(2, '0');
-    const formattedMonth = (month + 1).toString().padStart(2, '0'); 
-
-    return `${day}-${formattedMonth}-${s}`;
-}
-//  console.log(firstDayWeek(1, '1000')); //'01-01-1000'
-// console.log(firstDayWeek(2, '0001')); //'08-01-0001'
-// console.log(firstDayWeek(52, '1000')); //'22-12-1000'
-// console.log(firstDayWeek(43, '1983')); //'17-10-1983'
