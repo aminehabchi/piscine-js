2750575ea01c1ff271ea4ec410cc4fb2550a4c67
ahabchi <amine.habchi.01@gmail.com>
2024-10-08 11:45:40 +0100

    js

---
+++ get-some-time.js
@@ -0,0 +1,19 @@
+function firstDayWeek(n,s){
+    n=n*7 -6
+    m=[31,28,31,30,31,30,31,31,30,31,30,31]
+    j=0
+    while (n>m[j]){
+        n=n-m[j]
+        j++
+    }
+    j+=1
+    if (j<10){
+        j="0"+j
+    }
+    if (n<10){
+        n="0"+n
+    }
+    return n+":"+j+":"+s
+}
+
+console.log(firstDayWeek(5,'2020'))
