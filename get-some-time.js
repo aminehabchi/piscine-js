8f1ba6f3ad267df7be96681392df319ca50d8986
ahabchi <amine.habchi.01@gmail.com>
2024-10-08 12:39:36 +0100

    d

--- get-some-time.js
+++ get-some-time.js
@@ -1,33 +1,41 @@
 function firstDayWeek(n,s){
     n=n*7 
     const m=[31,28,31,30,31,30,31,31,30,31,30,31]
-    if (isLeapYear(s)){
-        m[1]=29
-    }
+    // if (isLeapYear(s)){
+    //     m[1]=29
+    // }
+    const d=new Date()
+    d.setDate(1)
+    d.setMonth(1)
+    d.setFullYear(s)
+   let da =d.getDay()
+   n=n-da-2
+// console.log(da)
     let j=0
     while (n>m[j]){
+        if (n-m[j]<0){
+            break
+        }
         n=n-m[j]
         j++
     }
-    if (n<0){
-        n*=-1
-    }
     j+=1
-    const date=new Date()
-    date.setDate(n)
-    date.setMonth(j)
-    date.setFullYear(s)
-   let day =n-date.getDay()-3
-    if (day<0){
-        if (j==1){
-            n=1
-        }else{
-            j--
-            n=m[j]+n
-        }
-    }else{
-        n=day
-    }
+//     const date=new Date()
+//     date.setDate(n)
+//     date.setMonth(j)
+//     date.setFullYear(s)
+//    let day =date.getDay()
+//    console.log(day,n)
+//     if (day<0){
+//         if (j==1){
+//             n=1
+//         }else{
+//             j--
+//             n=m[j]+n
+//         }
+//     }else{
+//         n=n-day
+//     }
     if (n<10){
         n="0"+n
     }
