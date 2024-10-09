69ad74af4dd95e6e9763de3a108ff7bd14fe9467
ahabchi <amine.habchi.01@gmail.com>
2024-10-09 12:50:53 +0100

    s

--- using-filter.js
+++ using-filter.js
@@ -1,27 +1,8 @@
-function filterShortStateName(arr){
-    return arr.filter((s)=>s.length<=7)
-}
-function filterStartVowel(arr){
-    return arr.filter((s) => checkV(s[0]))
-}
-
-function filter5Vowels(arr){
-    return arr.filter((s)=>check(s,5))
-}
-function checkV(s){
-    if (s=='a' || s=='e' || s=='i' || s=='o' || s=='u'){
-        return true
-    }
-    if (s=='A' || s=='E' || s=='I' || s=='O' || s=='U'){
-        return true
-    }
-    return false
-}
-function check(s,k){
+function countV(s,k){
     let c=0
     s=s.split('')
     for (let i=0;i<s.length;i++){
-        if (checkV(s[i])){
+        if (Vowels(s[i])){
             c++
         }
     }
@@ -30,37 +11,31 @@ function check(s,k){
     }
     return false
 }
-function filter1DistinctVowel(arr){
-    return arr.filter((s)=> check2(s))
-}
-function check2(s){
+function duplicate(s){
     if (s==''){
         return false
     }
     let v=''
     for (let i=0;i<s.length;i++){
-        if (v!='' && checkV(s[i]) && s[i].toLowerCase()!=v){
+        if (v!='' && Vowels(s[i]) && s[i].toLowerCase()!=v){
             return false
         }
-        if (checkV(s[i])){
+        if (Vowels(s[i])){
             v=s[i].toLowerCase()
         }
     }
     return true
 }
-function multiFilter(arr){
-    return arr.filter((o)=>check3(o))
-}
-function check3(obj){
+function isValid(obj){
     if (obj.capital.length<8){
         return false
     }
-    if (checkV(obj.name[0])==false){
+    if (Vowels(obj.name[0])==false){
         return false
     }
     let bl=false
     for (let i=0;i<obj.tag;i++){
-        if (checkV(obj.tag[i])){
+        if (Vowels(obj.tag[i])){
             bl=true
         }
     }
@@ -71,5 +46,33 @@ function check3(obj){
     if (obj.region=="South"){
         return false
     }
+}
+
+function filterShortStateName(arr){
+    return arr.filter((s)=>s.length<=7)
+}
+function filterStartVowel(arr){
+    return arr.filter((s) => Vowels(s[0]))
+}
+
+function filter5Vowels(arr){
+    return arr.filter((s)=>countV(s,5))
+}
+function Vowels(s){
+    if (s=='a' || s=='e' || s=='i' || s=='o' || s=='u'){
+        return true
+    }
+    if (s=='A' || s=='E' || s=='I' || s=='O' || s=='U'){
+        return true
+    }
+    return false
+}
+
+function filter1DistinctVowel(arr){
+    return arr.filter((s)=> duplicate(s))
+}
+
+function multiFilter(arr){
+    return arr.filter((o)=>isValid(o))
 }
 
