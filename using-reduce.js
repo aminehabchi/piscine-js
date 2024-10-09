3830dbfb6b36bbbd94a023c39ccbab80b6dbc7b1
ahabchi <amine.habchi.01@gmail.com>
2024-10-09 13:57:59 +0100

    s

--- using-reduce.js
+++ using-reduce.js
@@ -1,21 +1,10 @@
-function adder(arr, st = 0) {
-    return arr.reduce((s, n) => s + n, st)
+const adder = (arr, i) => arr.reduce((a, c) => a + c, i ?? 0)
+
+function sumOrMul(arr, i ) {
+
+    const out = arr.reduce((a, c) => c % 2 == 0 ? a * c : a + c, i??0)
+    return out
 }
-function sumOrMul(arr, sum) {
-    if (typeof(sum) != 'undefined') {
-        if (arr[0] % 2 == 0) {
-            arr[0] *= sum
-        } else {
-            arr[0] += sum
-        }
-    }
-    return arr.reduce((s, n) => {
-        if (n % 2 == 0) {
-            s *= n
-        } else {
-            s += n
-        }
-        return s
-    })
-}
-// const funcExec = (arr, i) => arr.reduce((a, c) => c(a), i??0)
+
+
+const funcExec = (arr, i) => arr.reduce((a, c) => c(a), i??0)
