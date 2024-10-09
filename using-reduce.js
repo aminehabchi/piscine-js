26fba811bd553ba4004aea2088f94c7d58afb3f6
ahabchi <amine.habchi.01@gmail.com>
2024-10-09 13:39:55 +0100

    js

--- using-reduce.js
+++ using-reduce.js
@@ -1,22 +1,24 @@
-function adder(arr,st=0){
-    return arr.reduce((s,n)=>s+n,st)
+function adder(arr, st = 0) {
+    return arr.reduce((s, n) => s + n, st)
 }
-function sumOrMul(arr,sum){
-    if (arr[0]%2==0){
-        arr[0]*=sum
-    }else{
-        arr[0]+=sum
+function sumOrMul(arr, sum) {
+    if (typeof(sum) != 'undefined') {
+        if (arr[0] % 2 == 0) {
+            arr[0] *= sum
+        } else {
+            arr[0] += sum
+        }
     }
-    return arr.reduce((s,n)=>{
-        if (n%2==0){
-            s*=n
-        }else{
-            s+=n
+    return arr.reduce((s, n) => {
+        if (n % 2 == 0) {
+            s *= n
+        } else {
+            s += n
         }
         return s
     })
 }
-function funcExec(arr){
+function funcExec(arr) {
     return 0
 }
-// console.log(adder([9, 24, 7, 11, 3]));
+// console.log(sumOrMul([29, 23, 3, 2, 25]));
