09fcffe40349b63060991cf00d33a0599641b71f
ahabchi <amine.habchi.01@gmail.com>
2024-10-10 11:07:39 +0100

    done

--- has-city.js
+++ has-city.js
@@ -1,10 +1,13 @@
 function hasCity(country,arr){
     return function func(city){
-        return arr.some((element)=>{
-            if (element===city){
-                return city +' is a city from '+country
-            }
-            return city +' is not a city from '+country
-        })
+        return some(arr,city,country)
     }
+}
+function some(arr,city,country){
+    for (let i=0;i<arr.length;i++){
+        if (arr[i]==city){
+           return city +' is a city from '+country
+        }
+    }
+    return city +' is not a city from '+country
 }
--- quantifiers.js
+++ quantifiers.js
@@ -6,14 +6,7 @@ function every(arr,func){
     }
     return true
 }
-function some(arr,func){
-    for (let i=0;i<arr.length;i++){
-        if (func(arr[i],i,arr)){
-           return true
-        }
-    }
-    return false
-}
+
 function none(arr,func){
     for (let i=0;i<arr.length;i++){
         if (func(arr[i],i,arr)){
