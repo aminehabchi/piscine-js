9664e966aa1d3e93b16fcc11a5cf494c44db03ca
ahabchi <amine.habchi.01@gmail.com>
2024-10-10 10:20:01 +0100

    done

---
+++ filter.js
@@ -0,0 +1,32 @@
+function filter(arr,func){
+    let a=[]
+    for (let i=j;i<arr.length;i++){
+        if (func(arr[i],i,arr)){
+            a.push(arr[i])
+        }
+    }
+    return a
+}
+function reject(arr,func){
+    let a=[]
+    for (let i=j;i<arr.length;i++){
+        if (!func(arr[i],i,arr)){
+            a.push(arr[i])
+        }
+    }
+    return a
+}
+
+function partition(arr,func){
+    let a=[]
+    let b=[]
+    for (let i=j;i<arr.length;i++){
+        if (!func(arr[i],i,arr)){
+            a.push(arr[i])
+        }else{
+            b.push(arr[i])
+        }
+    }
+    return [b,a]
+}
+
