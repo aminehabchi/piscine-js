07911b8116ed4d92a35cf0989a346bdd47194632
amine habchi <ahabchi@noreply.learn.zone01oujda.ma>
2024-10-11 09:48:13 +0000

    Update 'get-them-all.js'

--- get-them-all.js
+++ get-them-all.js
@@ -1,39 +1,26 @@
+export const getArchitects = () => {
 
-export function getArchitects() {
-    
-    const architects = Array.from(people.getElementsByTagName('a'));
-
-    const allPeople = Array.from(people.getElementsByTagName('*')); 
-
-    const nonArchitects = allPeople.filter((person) => person.tagName.toLowerCase() !== 'a');
-
-    return [architects, nonArchitects];
+   const architects = Array.from(document.getElementsByTagName('a'));
+    const nonArchitects = Array.from(document.getElementsByTagName('span')); 
+   return [architects, nonArchitects];
 }
 
-export function getClassical(){
-    const allPeople = getArchitects()[0]
-    const classical = allPeople.filter(person => person.className.toLowerCase() === 'classical');
-    const nonClassical = allPeople.filter(person => person.className.toLowerCase() !== 'classical');
-
-    return [classical, nonClassical];
+export const getClassical = () => {
+    const classical=getArchitects()[0]
+    const nonclassical=classical.filter((person) => person.tagName.toLowerCase() !== 'classical');
+    return [ classical, nonclassical ];
 }
-export function getActive(){
-    const allPeople = Array.from(document.getElementsByClassName('*')); 
 
-    const active =  allPeople.filter(person => person.className.toLowerCase() === 'active');
-
-    const nonActive = allPeople.filter(person => person.className.toLowerCase() !== 'active');
-
-    return [active, nonActive];
-
+export const getActive = () => {
+    return [
+        document.querySelectorAll("a.classical.active"),
+        document.querySelectorAll("a.classical:not(.active)"),
+    ];
 }
-// export function getBonannoPisano(){
-      
-//     const architects = Array.from(document.getElementsByTagName('BonannoPisano'));
 
-//     const allPeople = Array.from(document.getElementsById('*')); 
-
-//     const nonArchitects = allPeople.filter(person => person.t.toLowerCase() !== 'BonannoPisano');
-
-//     return [architects, nonArchitects];
-// }
+export const getBonannoPisano = () => {
+    return [
+        document.getElementById("BonannoPisano"),
+        getActive()[0],
+    ];
+}
