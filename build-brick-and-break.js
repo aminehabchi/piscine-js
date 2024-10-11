7b0dd9f7685d14b48bb70a1b1725e6b8cbe5d376
ahabchi <amine.habchi.01@gmail.com>
2024-10-11 13:29:52 +0100

    done

---
+++ build-brick-and-break.js
@@ -0,0 +1,40 @@
+export function build(n) {
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('id', 'brick-' + (i + 1));
+    if ((i + 1) % 3 === 2 ){
+        newDiv.setAttribute("foundation", "true")
+    } else {
+        newDiv.setAttribute("foundation", null)
+    }
+
+    document.body.appendChild(newDiv);
+    newDiv.innerHTML = i + 1
+    n--
+    i++
+    if (n > 0) {
+        setTimeout(() => {
+            build(n);
+        }, 100);
+    }
+}
+
+export function repair(...ids) {
+    ids.forEach((id) => {
+        let brick = document.getElementById(id);
+        let a= brick.getAttribute("foundation")
+  
+        if (a=="true"){
+            brick.innerHTML=id.split('-')[1]+'-in progress'
+        } else{
+            brick.innerHTML=id.split('-')[1]+'-repaired'
+        }
+    });
+}
+let i = 0
+export function destroy() {
+    if (i != 0) {
+        const element = document.getElementById('brick-' + i);
+        element.remove();
+        i--
+    }
+}
