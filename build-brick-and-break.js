dbfe6585e76256d2e9ca51c3556a060139c12618
amine habchi <ahabchi@noreply.learn.zone01oujda.ma>
2024-10-12 08:26:36 +0000

    Update 'build-brick-and-break.js'

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,16 +1,20 @@
+let i = 0;
+
 export function build(n) {
-    const newDiv = document.createElement('div');
-    newDiv.setAttribute('id', 'brick-' + (i + 1));
-    if ((i + 1) % 3 === 2 ){
-        newDiv.setAttribute("foundation", "true")
-    }
-    document.body.appendChild(newDiv);
-    newDiv.innerHTML = i + 1
-    n--
-    i++
     if (n > 0) {
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('id', 'brick-' + (i + 1));
+        
+        if ((i + 1) % 3 === 2) {
+            newDiv.dataset.foundation = true;
+        } else {
+            newDiv.removeAttribute('foundation');
+        }
+        newDiv.innerHTML = i + 1;
+        document.body.appendChild(newDiv);
+        i++;
         setTimeout(() => {
-            build(n);
+            build(n - 1); 
         }, 100);
     }
 }
@@ -18,20 +22,21 @@ export function build(n) {
 export function repair(...ids) {
     ids.forEach((id) => {
         let brick = document.getElementById(id);
-        let a= brick.getAttribute("foundation")
-  
-        if (a=="true"){
-            brick.setAttribute("data-repaired", 'in progress')
-        } else{
-            brick.setAttribute("data-repaired", "true")
+        if (brick) {
+            if (brick.dataset.foundation) {
+                brick.dataset.repaired = 'in progress';
+            } else {
+                brick.dataset.repaired = 'true';
+            }
         }
     });
 }
-let i = 0
 export function destroy() {
     if (i != 0) {
         const element = document.getElementById('brick-' + i);
-        element.remove();
-        i--
+        if (element) {
+            element.remove();
+            i--;
+        }
     }
 }
