92aa3fe49e286b98f53b1a91a893b46780e8a9e2
ahabchi <amine.habchi.01@gmail.com>
2024-10-14 09:42:49 +0100

    done

--- invert.js
+++ invert.js
@@ -1,8 +1,18 @@
 function invert(obj) {
-    let newObj = {}
-    for (const key in obj) {
-        newObj[obj[key]]=key
+     let newObj = {}
+   
+    let keys=Object.keys(obj);
+    let values=Object.values(obj);
+
+    for (let i=0;i<keys.length && i<values.length;i++){
+   
+        newObj[values[i]]=keys[i]
+
     }
+
+
     return newObj
 }
+
 // console.log(invert({ language: 'english' }));
+// console.log(invert({ f: 5, __proto__: { d: 6 } }));
