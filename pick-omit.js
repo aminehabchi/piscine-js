2b7e35d7b686cb436babd3a16e939eed222ee418
ahabchi <amine.habchi.01@gmail.com>
2024-10-14 09:58:38 +0100

    done

--- pick-omit.js
+++ pick-omit.js
@@ -1,30 +1,32 @@
 function pick(obj, ...arr) {
-    let appear, _ = getKey(obj, arr)
-    let o = {}
+    let { appear } = getKey(obj, arr);
+    let o = {};
     for (let i = 0; i < appear.length; i++) {
-        o[appear[i]] = obj[appear[i]]
+        o[appear[i]] = obj[appear[i]];
     }
-    return o
+    return o;
 }
+
 function omit(obj, ...arr) {
-    let _, notappear = getKey(obj, arr)
-    let o = {}
+    let { notappear } = getKey(obj, arr);
+    let o = {};
     for (let i = 0; i < notappear.length; i++) {
-        o[notappear[i]] = obj[notappear[i]]
+        o[notappear[i]] = obj[notappear[i]];
     }
-    return o
+    return o;
 }
-function getKey(obj, ...arr) {
-    let appear = []
-    let notappear = []
-    let keys = Object.keys(obj)
+
+function getKey(obj, arr) {
+    let appear = [];
+    let notappear = [];
+    let keys = Object.keys(obj);
 
     for (let i = 0; i < keys.length; i++) {
         if (arr.includes(keys[i])) {
-            appear.push(keys[i])
+            appear.push(keys[i]);
         } else {
-            notappear.push(keys[i])
+            notappear.push(keys[i]);
         }
     }
-    return appear, notappear
+    return { appear, notappear };
 }
