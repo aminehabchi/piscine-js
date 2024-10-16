c3a8d6c080967bdd8dacac957a9abf4245b98661
ahabchi <amine.habchi.01@gmail.com>
2024-10-16 13:03:01 +0100

    done

--- deep-copy.js
+++ deep-copy.js
@@ -1,4 +1,17 @@
 function deepCopy(obj,copy={}){
+    if (Array.isArray(obj)){
+        if (copy.length==0){
+            copy=[]
+        }
+        for (let i=0;i<obj.length;i++){
+            if (typeof obj[i]=="object"){
+                copy=deepCopy(obj[i],copy)
+            }else{
+                copy.push(obj[i])
+            }
+        }
+        return copy
+    }
     let keys=Object.keys(obj)
     for (let i=0;i<keys.length;i++){
         if (typeof obj[keys[i]]=="object"){
