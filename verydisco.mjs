53582e16ad1895ed695e7102b6df8c53d5df88d4
ahabchi <amine.habchi.01@gmail.com>
2024-10-21 12:01:52 +0100

    done

--- verydisco.mjs
+++ verydisco.mjs
@@ -8,7 +8,9 @@ for (let j = 2; j < argv.length; j++) {
     for (let i = 0; i < val.length; i++) {
         let l = Math.ceil(val[i].length / 2)
         s = s + val[i].slice(l, val[i].length) + val[i].slice(0, l)
-
+        if (i != val.length - 1) {
+            s = s + " "
+        }
     }
 
     if (j != argv.length - 1) {
@@ -17,4 +19,5 @@ for (let j = 2; j < argv.length; j++) {
 }
 
 console.log(s);
+
 
