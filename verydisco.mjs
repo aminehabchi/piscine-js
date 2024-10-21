4e1b75951f4baf8dca9e26b408393ecdfbd50724
ahabchi <amine.habchi.01@gmail.com>
2024-10-21 12:21:09 +0100

    done

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,22 +1,21 @@
 import { argv } from 'node:process';
 let s = ""
 // print process.argv
-for (let j = 2; j < argv.length; j++) {
-    let val = argv[j]
-    val = val.split(' ')
 
-    for (let i = 0; i < val.length; i++) {
-        let l = Math.ceil(val[i].length / 2)
-        s = s + val[i].slice(l, val[i].length) + val[i].slice(0, l)
-        if (i != val.length - 1) {
-            s = s + " "
-        }
-    }
+let val = argv[2]
 
-    if (j != argv.length - 1) {
+val = val.split(' ')
+
+for (let i = 0; i < val.length; i++) {
+    let l = Math.ceil(val[i].length / 2)
+    s = s + val[i].slice(l, val[i].length) + val[i].slice(0, l)
+    if (i != val.length - 1) {
         s = s + " "
     }
 }
+
+
+
 
 console.log(s);
 
