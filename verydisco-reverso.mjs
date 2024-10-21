efff4ae354474e8dbd2ff5be409c932161eeb305
ahabchi <amine.habchi.01@gmail.com>
2024-10-21 12:52:09 +0100

    doen

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -1,23 +1,27 @@
 import { argv } from 'node:process';
 import fs from 'fs';
 let s = ""
-let filePath=argv[2]
+let filePath = argv[2]
 
 fs.readFile(filePath, 'utf8', (err, val) => {
     if (err) {
         console.error('Error reading the file:', err);
     } else {
         val = val.split(' ')
+
         for (let i = 0; i < val.length; i++) {
-            let l = Math.ceil(val[i].length / 2)
+            let l = Math.floor(val[i].length / 2)
             s = s + val[i].slice(l, val[i].length) + val[i].slice(0, l)
             if (i != val.length - 1) {
                 s = s + " "
             }
         }
+
         console.log(s)
     }
 });
+
+
 
 
 
