12475a4b2e1b1f1a7a88890273b4253d926ad2e8
ahabchi <amine.habchi.01@gmail.com>
2025-03-16 10:46:32 +0000

    done

--- verydisco.mjs
+++ verydisco.mjs
@@ -5,15 +5,17 @@ if (args && args.length > 0) {
     let s = args[0]
 
     let arr = s.split(" ")
+    let ss = ""
     for (let i in arr) {
         let l = arr[i].length
         let half = Math.floor(l / 2)
 
         if (l % 2 == 0) {
-            console.log(arr[i].slice(half, l) + arr[i].slice(0, half));
+            arr[i] = (arr[i].slice(half, l) + arr[i].slice(0, half));
         } else {
-            console.log(arr[i].slice(half + 1, l) + arr[i].slice(0, half + 1));
+            arr[i] = (arr[i].slice(half + 1, l) + arr[i].slice(0, half + 1));
         }
     }
+    console.log(arr.join(" "));
 
 }
